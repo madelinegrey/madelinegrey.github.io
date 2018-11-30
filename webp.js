e325ff703f39aedcc8da8aec844fbbbb0e277dd5
Maddie Grey <madelinegrey@maddies-mac-2.lan>
2018-11-30 17:31:53 -0500

    nodes and web

--- webp.js
+++ webp.js
@@ -1,4 +1,4 @@
-var imagemin = require("imagemin"),    // The imagemin module.
+/* var imagemin = require("imagemin"),    // The imagemin module.
   webp = require("imagemin-webp"),   // imagemin's WebP plugin.
   outputFolder = "./images",            // Output folder
   PNGImages = "./images/*.png",         // PNG images
@@ -14,4 +14,15 @@ imagemin([JPEGImages], outputFolder, {
   plugins: [webp({
     quality: 65 // Quality setting from 0 to 100
   })]
+}); */
+
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+
+imagemin(['images/*.{jpg,png}'], 'images', {
+    use: [
+        imageminWebp({quality: 60})
+    ]
+}).then(() => {
+    console.log('Images optimized');
 });
